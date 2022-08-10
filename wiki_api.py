ba9ee501bce9c9de5b08ac4b5ccc28915a95b591
aclozovoy <aclozovoy@gmail.com>
2022-08-10 13:48:10 -0700

    JSON data to DataFrame

--- wiki_api.py
+++ wiki_api.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import pandas as pd
 
  
 subject = 'Python (programming language)'
@@ -11,6 +13,30 @@ params = {
         'pvipdays': 60,
     }
  
-response = requests.get(url, params=params)
-data = response.json()
-print(data)
+r = requests.get(url, params=params)
+data = r.json()
+
+# Page id
+temp = data['query']['pages']
+pageid = list(temp.keys())[0]
+
+# Title
+title = data['query']['pages'][str(pageid)]['title']
+
+# Viewcount data
+clean = data['query']['pages'][str(pageid)]['pageviews']
+print(clean)
+
+# Move data from dictionary to DataFrame
+df = pd.DataFrame(clean.items(), columns = ['Date', 'Views'])
+print(df)
+
+
+
+
+
+
+# url = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/Albert_Einstein/daily/2013100100/2015103100'
+# df2 = pd.read_json(url)
+# print(df2)
+
